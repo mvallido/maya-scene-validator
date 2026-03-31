072729de4cc462dd2b6df50a3d00114ac3f8c837
Michael Vallido <61817636+mvallido@users.noreply.github.com>
2026-03-31 10:35:31 -0700

    refactor(validation): update main guards

--- cz_validation/test_cases/template_test.py
+++ cz_validation/test_cases/template_test.py
@@ -34,10 +34,13 @@ class TemplateTest(TestCase):
 
 
 if __name__ == "__main__":
-    tc = TestCase()
-
+    tc = TemplateTest()
     success = tc.run_test()
-    if not success and tc.is_select_errors_enabled():
-        tc.select_error_objs()
 
     print(tc.formatted_results())
+    if not success:
+        if tc.is_select_errors_enabled():
+            tc.select_error_objs()
+
+        if tc.is_fix_errors_enabled():
+            tc.fix_errors()
